76851ce496eb70ec55910db98bdb28394ea38414
EricaMartine <ericamtz31@hotmail.com>
2021-12-09 00:06:08 -0600

    added analysis for evals

---
+++ analysis.py
@@ -0,0 +1,39 @@
+import json
+from collections import Counter
+
+# (label 0 is "entailed", 1 is "neutral", and 2 is "contradiction")
+def analyze_eval():
+
+    #opening eval file
+    file_name = "no_premise_eval_output/eval_predictions.jsonl"
+    data = open(file_name)
+
+    #declare vars
+    truth_counter = Counter()
+    pred_counter = Counter()
+    stats = Counter()
+
+    #populate vars
+    for datum in data:
+        example = json.loads(datum)
+        truth = example['label']
+        pred = example['predicted_label']
+        truth_counter[truth] += 1
+        pred_counter[pred] += 1
+        stats[(pred, truth == pred)] += 1
+
+        
+        
+
+    #print results
+    print("gold labels")
+    print(truth_counter)
+    print()
+    print("predictions")
+    print(pred_counter)
+    print()
+    print("stats")
+    print(stats)
+
+if __name__ == "__main__":
+    analyze_eval()
